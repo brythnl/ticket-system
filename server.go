ec3e4e124805de2d0f27626e8b90f92b4a1ba1c0
Bryan <bryanathanaelj@gmail.com>
2023-12-20 00:23:12 +0100

    feature: setup basic server and WebSocket connection/endpoint

--- server.go
+++ server.go
@@ -1,2 +1,34 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+  "log"
+
+  "github.com/gorilla/websocket"
+)
+
+var (
+  upgrader = websocket.Upgrader{
+    ReadBufferSize: 1024,
+    WriteBufferSize: 1024,
+  }
+)
+
+func ws(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Hello, Go Server")
+
+  conn, err := upgrader.Upgrade(w, r, nil)
+  if err != nil {
+    log.Println(err)
+    return
+  }
+
+}
+
+func main() {
+  http.HandleFunc("/ws", ws)
+  http.ListenAndServe(":8080", nil)
+}
 
 
